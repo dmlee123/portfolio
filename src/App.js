10652517816bf26c57866c25116c51f34a5e5ad6
Daniel Lee <daniellee@Daniels-MacBook-Air.local>
2022-02-03 14:34:41 -0800

    added basic layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import Navigate from './Components/Navigate/Navigate';
+import { Link, Routes, Route } from 'react-router-dom';
 
 function App() {
-	return <div className='App'>hello world</div>;
+	return <div className='App'>
+      <div>
+        <h1> Hello!</h1>
+        <h3>Daniel Lee  Software Developer </h3>
+      </div>
+    <Navigate />
+    </div>;
 }
 
 export default App;
---
+++ src/Components/Navigate/Navigate.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./navigate.css"
+
+function Navigate() {
+  return (
+		<div>
+			<button id='button'>Work</button>
+			<button id='button'>About</button>
+			<button id='button'>Contact</button>
+		</div>
+	);
+}
+
+export default Navigate;
