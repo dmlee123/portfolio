bae0ccfe7ee8e46fd5bc456e2d6ca0bc549d8988
Daniel Lee <daniellee@Daniels-Air.attlocal.net>
2022-02-09 20:03:21 -0800

    added typewriter javascript

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,11 +1,28 @@
 import React from 'react';
-import "./home.css"
+import './home.css';
+import Typewriter from 'typewriter-effect';
 
 function Home() {
 	return (
 		<div>
 			<div>
-				<h1> Hello!</h1>
+				<Typewriter
+					options={{loop: true, }}
+					onInit={(typewriter) => {
+						typewriter
+							.typeString('Hello!')
+							.pauseFor(2200)
+							.deleteAll(300)
+							.pauseFor(500)
+							.typeString('Welcime')
+							.pauseFor(100)
+							.deleteChars(5)
+							.typeString('lcome!')
+							.pauseFor(2200)
+							.deleteAll(150)
+							.start();
+					}}
+				/>
 				<h3>Daniel Lee Software Developer </h3>
 			</div>
 		</div>
